6cdba979e531b6805f90603ab742843b313703c4
0x384c0 <0x384c0@gmail.com>
2020-05-06 16:03:46 +0600

    added navigation

--- Readme.md
+++ Readme.md
@@ -4,6 +4,7 @@ react-native init example
 cd example
 npm install @react-navigation/native
 npm install react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view
+npm install @react-navigation/stack
 npx pod-install ios
 react-native run-ios
 ```
--- example/App.js
+++ example/App.js
@@ -6,14 +6,81 @@
  * @flow strict-local
  */
 
-import React from 'react';
 
-
-import 'react-native-gesture-handler';
+import * as React from 'react';
+import { Button, View, Text, FlatList, StyleSheet,TouchableHighlight } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
-    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <FlatList
+          data={[
+            {key: 'NavigationScreen1'},
+            {key: 'Rest api wiki'},
+            {key: 'Rest api google images'},
+            {key: 'Map'},
+          ]}
+          renderItem={({item}) => <TouchableHighlight onPress={() => navigation.navigate(item.key)}>
+            <Text style={styles.item}>{item.key}</Text>
+            </TouchableHighlight>
+            }
+        />
+    </View>
   );
-};
+}
+const styles = StyleSheet.create({
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
+
+
+function NavigationScreen1({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>NavigationScreen1 Screen</Text>
+      <Button
+        title="Go to NavigationScreen2"
+        onPress={() => navigation.navigate('NavigationScreen2')}
+      />
+      </View>
+      );
+}
+
+function NavigationScreen2({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>NavigationScreen2 Screen</Text>
+      <Button
+        title="Go to NavigationScreen2... again"
+        onPress={() => navigation.push('NavigationScreen2')}
+      />
+      <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button
+        title="Go back to first screen in stack"
+        onPress={() => navigation.popToTop()}
+      />
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="NavigationScreen1" component={NavigationScreen1} options={{headerBackTitleVisible:false}} />
+        <Stack.Screen name="NavigationScreen2" component={NavigationScreen2} options={{headerBackTitleVisible:false}} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
